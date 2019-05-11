d0fa67b10989648fde7ca7c9a157380247df2ce6
Adriano <adriano.selaviles@gmail.com>
2019-05-11 16:08:06 -0600

    latest

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 sample:
 	mkdir out
-	go run cmd/sample.go 0 face_algos/haarcascade_frontalface_default.xml 10 ./out
+	go run cmd/cmd.go sample 0 face_algos/haarcascade_frontalface_default.xml 10 ./out
+
+train:
+	go run cmd/cmd.go train http://localhost:8080 ./out
 
 clean:
 	rm -rf out
---
+++ cmd/cmd.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/adrianosela/GoFaceTrainer/sampler"
+	"github.com/adrianosela/GoFaceTrainer/trainer"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatal("usage: go run cmd.go [train/sample] ...args")
+	}
+
+	switch os.Args[1] {
+	case "sample":
+		var set sampler.Settings
+		var err error
+
+		if len(os.Args) != 6 {
+			log.Fatal("usage: go run cmd.go sample <device_id> <face_algo_path> <nsamples> <save_dir>")
+		}
+
+		if set.CaptureDeviceID, err = strconv.Atoi(os.Args[2]); err != nil {
+			log.Fatalf("capture device not an integer: %s", err)
+		}
+		set.FaceAlgoPath = os.Args[3]
+		if set.NSamples, err = strconv.Atoi(os.Args[4]); err != nil {
+			log.Fatalf("nsamples not an integer: %s", err)
+		}
+		set.SaveSamplesDir = os.Args[5]
+
+		s, err := sampler.NewFaceSampler(set)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer s.Close()
+		s.Run()
+		return
+	case "train":
+		var set trainer.Settings
+		var err error
+
+		if len(os.Args) != 4 {
+			log.Fatal("usage: go run cmd.go train <facebox_url> <faces_src_dir>")
+		}
+
+		set.FaceboxAddress = os.Args[2]
+		set.FacesSrcDir = os.Args[3]
+
+		t, err := trainer.NewFaceTrainer(set)
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.Run()
+		return
+	default:
+		log.Fatal("usage: go run cmd.go [train/sample] ...args")
+	}
+}
