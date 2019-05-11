79f4c949f0ead5970895463732f680ea3bb28e1e
Adriano <adriano.selaviles@gmail.com>
2019-05-10 21:16:08 -0600

    add init go file

---
+++ trainer/trainer.go
@@ -0,0 +1,14 @@
+package trainer
+
+// FaceTrainer is an abstraction around the necessary OpenCV and MachineBox
+// resources in order to produce a model of a face or facial-expression
+type FaceTrainer struct {
+	// TODO
+}
+
+// NewFaceTrainer is the FaceTrainer constructor
+func NewFaceTrainer( /* TODO */ ) (*FaceTrainer, error) {
+	return &FaceTrainer{
+		// TODO
+	}, nil
+}
